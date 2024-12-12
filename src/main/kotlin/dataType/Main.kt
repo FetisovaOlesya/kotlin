b06dc291426c6769098fe33cebc7763dcfa0ee52
fetisova1 <fetisova@individ.ru>
2024-12-12 23:12:30 +0300

    Домашнее задание по теме "Переменные и типы данных"

---
+++ src/main/kotlin/dataType/Main.kt
@@ -0,0 +1,10 @@
+package dataType
+
+fun main(args: Array<String>) {
+    val age: Int = 40
+    val name: String = "Олеся"
+    val isStudent: Boolean = true
+    val salary: Double = 160000.0
+
+    println("Age: " + age + " | Name: " + name + " | Is Student: " + isStudent + " | Salary: " + salary)
+}
